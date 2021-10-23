ce568bcb843c3191e139133db8e76eab92dd9674
Raajokiaa-Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2021-10-23 09:57:26 +0600

    Update dijkstraAlgo.cpp

--- dijkstraAlgo.cpp
+++ dijkstraAlgo.cpp
@@ -1,98 +1,101 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-#define ll long long
+#define pb push_back
 #define inf 1e9
 #define pii pair<int,int>
 
+int i,j,node,edges,u,v,w,cost,wgt;
+int source,destination;
+
+vector<pair<int,int>>Graph[1000];
+priority_queue<pii,vector<pii>,greater<pii>>PQ; //distance,source 
 
 
-int i,j,node,edge,u,v,w,wgt,cost;
-int source,destination;//Starting point of Traversal
-
-
-vector<pair<int,int>>Graph[100005];
-priority_queue<pii,vector<pii>,greater<pii>>PQ; //Min Heap
-
-
-vector<int>Parent;
 vector<int>Dist;
+vector<int>Parent;
 
 void init()
 {
-
-	Parent.assign(node+1,-1);
 	Dist.assign(node+1,inf);
+	Parent.assign(node+1,-1);
 
-	Dist[source]    = 0;
-
+	Dist[source] = 0;
 }
 
 void Dijkstra()
 {
 	init();
-
-	PQ.push({Dist[source],source});//(distance,SourceNode);
+	PQ.push({Dist[source],source});
 
 	while(!PQ.empty())
 	{
-		cost = PQ.top().first;
-		u    = PQ.top().second;
-		PQ.pop();				
-        
-        for(auto child : Graph[u])
+		cost  = PQ.top().first;
+		u     = PQ.top().second;
+
+		PQ.pop();
+
+		vector<pair<int,int>>::iterator it;
+		for(it= Graph[u].begin(); it!=Graph[u].end(); it++)		
 		{
-			v   = child.first;
-			wgt = child.second;
+			v   = it->first;
+			wgt = it->second;
 
-			if(cost + wgt < Dist[v])
+			if(cost + wgt <Dist[v])
 			{
-				Dist[v] = cost+wgt;
+				Dist[v] =cost+wgt;
 				Parent[v] = u;
-				PQ.push({Dist[v],v}); //distance,node
+				PQ.push({Dist[v],v});
 			}
-		}
 
+		}
 	}
+	
 }
 
-void Print_Shortest_Path()
+void print()
 {
-	cout<<"Distance from source: \n";
-	for(i=1;i<node;i++)
-		cout<<source<<"->"<<i<<" : "<<Dist[i]<<'\n';
-	cout<<endl;
-
-	vector<int>Path;
+	for(i=0;i<node;i++)
+	{
+		cout<<source<<" - "<<i<<" : "<<Dist[i]<<'\n';
+	}
+ 
 	int target = destination;
+    
+    vector<int>Path;
 	while(target != source)
 	{
 		Path.push_back(target);
 		target = Parent[target];
 	}
-    Path.push_back(source);
-    
-    cout<<"Shortest Path from "<<source<<" to "<<destination<<":"<<endl;
+	Path.push_back(source);
+	cout<<"\nshortest path:\n";
 	reverse(Path.begin(),Path.end());
-	for(auto it:Path)cout<<it<<' ';
+	for(auto it:Path)
+		cout<<it<<' ';
+    cout<<"\ncost : "<<Dist[destination]<<endl;
 
 }
-
 int main()
 {
-
-	cin>>node>>edge;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+	cin>>node>>edges;
 	cin>>source>>destination;
 
-	for(i=0;i<edge;i++)
-	{		
+	for(i=0;i<edges;i++)
+	{
 		cin>>u>>v>>w;
 		Graph[u].push_back({v,w});
-		Graph[v].push_back({u,w});
+	    Graph[v].push_back({u,w});	
 	}
-	
+
 	Dijkstra();
-	Print_Shortest_Path();
+	print();
+
+
+}
 
 /*
 
@@ -130,4 +133,4 @@ Shortest Path from 7 to 3
 7 6 5 2 3 
 
 */
-}
+
