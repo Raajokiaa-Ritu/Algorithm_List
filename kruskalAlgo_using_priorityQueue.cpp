c305b1c634f500e26217c64b0ee2952c9902d01f
Raajokiaa-Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2021-10-23 15:17:35 +0600

    Update kruskalAlgo_using_priorityQueue.cpp

--- kruskalAlgo_using_priorityQueue.cpp
+++ kruskalAlgo_using_priorityQueue.cpp
@@ -1,34 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define   pb   push_back
-#define   pii  pair<int,int>
+#define   pii  pair<int,pair<int,int>>
 #define   inf  100000
 
-int i,j,n,m,e,s,a,b,mincost=0;
-int w,node1,node2,node;
+int i,node,edges,mincost=0,source,destination;
+int u,v,w,a,b;
 
-unordered_map<int,int>v;
-vector<pair<int,pii>> mstset;
-vector<pair<int,pii>>g;
+unordered_map<int,int>parent;
+vector<pii> mstset;
 
-priority_queue<pair<int,pii>, vector<pair<int,pii>>, greater<pair<int,pii>>>pq;
+vector<pair<int,int>>Graph[1000];
+priority_queue<pii,vector<pii>,greater<pii>>pq;
 
 void make_set(int n)
 {
-  v[n]=n;
+  parent[n]=n;
 }
 ///path compression
 int findset(int n)
 {
-    if(v[n]==n)return n;
-    return v[n]=findset(v[n]);
+    if(parent[n]==n)return n;
+    return parent[n]=findset(parent[n]);
 }
 void union_set(int a,int b)
 {
     a=findset(a);
     b=findset(b);
     if(a<b)swap(a,b);
-    v[a]=b;
+    parent[a]=b;
 }
 
 int main()
@@ -38,10 +38,11 @@ int main()
    freopen("output.txt","w",stdout);
 #endif
 
-   cin>>n>>e;
-   for(i=1;i<=n;i++) make_set(i);
+   cin>>node>>edges;
+  
+   for(i=1;i<=node;i++) make_set(i);
    
-   for(i=0;i<e;i++)
+   for(i=0;i<edges;i++)
    {
       int u,v,w;
       cin>>u>>v>>w;
@@ -50,30 +51,63 @@ int main()
    
 
    while(!pq.empty())
-   {
-      
+   {      
       w = pq.top().first;
-      node1 = pq.top().second.first;
-      node2 = pq.top().second.second;
+      u = pq.top().second.first;
+      v = pq.top().second.second;
       pq.pop();
-      a = findset(node1);
-      b = findset(node2);
+      a = findset(u);
+      b = findset(v);
       if(a != b)
       {
-         mstset.pb({w,{node1,node2}});
+         mstset.pb({w,{u,v}});
          mincost += w;
-         union_set(node1,node2);
+         union_set(u,v);
       }  
    }
    cout<<"mincost : "<<mincost<<endl;
+
+   cout<<"\nMST SET :\n";
    sort(mstset.begin(),mstset.end());
    for(auto node : mstset)
    {
      w = node.first;
-     a = node.second.first;
-     b = node.second.second;
+     u = node.second.first;
+     v = node.second.second;
 
-     cout<<a<<" - "<<b<<" : "<<w<<endl;
+     cout<<u<<" - "<<v<<" : "<<w<<endl;
    }
 
 }
+/*
+input:gfg
+9 14
+0 1 4
+0 7 8
+1 2 8
+1 7 11
+2 3 7
+2 8 2
+2 5 4
+3 4 9
+3 5 14
+4 5 10
+5 6 2
+6 7 1
+6 8 6
+7 8 7
+
+output:
+mincost : 37
+
+MST SET :
+6 - 7 : 1
+2 - 8 : 2
+5 - 6 : 2
+0 - 1 : 4
+2 - 5 : 4
+2 - 3 : 7
+0 - 7 : 8
+3 - 4 : 9
+
+*/
