7f13502766b0b96da55a59bf1c8a7f1e9e8011e8
Raajokiaa-Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2021-10-24 11:33:06 +0600

    Add files via upload

---
+++ activity selection problem.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+
+int i,n;
+vector<int>SelectedActivity;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+	cin>>n;
+	int startTime[n+1];
+	int endTime[n+1];
+	for(i=1;i<=n;i++)cin>>startTime[i];
+	for(i=1;i<=n;i++)cin>>endTime[i];
+
+	
+	SelectedActivity.push_back(1);
+	int key = 1;
+
+	for(i=2;i<=n;i++)
+	{
+		if(startTime[i] >= endTime[key])
+		{
+			SelectedActivity.push_back(i);
+			key = i;
+		}
+	}
+    
+    cout<<"Activity: ";
+	for(i=0;i<SelectedActivity.size();i++)
+	{
+		cout<<"a"<<SelectedActivity[i]<<' ';
+	}
+}
