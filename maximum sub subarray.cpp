6db8a129f11315b764457775401574713b6bd0cc
Raajokiaa-Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2021-10-24 11:42:39 +0600

    Update maximum sub subarray.cpp

--- maximum sub subarray.cpp
+++ maximum sub subarray.cpp
@@ -1,77 +1,79 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define pb push_back
-
-int i,j,n,m,e,sum=0;
-int leftSum=0,rightSum=0,crossSum=0;
-
-int max_cross_sum(int arry[],int low,int mid,int high)
-{
-	int left = INT_MIN;
-	sum=0;
-
-	for(i=mid;i>=low;i--)
-	{
-		sum +=arry[i];
-		if(sum > left)
-		{
-			left= sum;
-		}else break;
-	}
-
-
-	int right = INT_MIN;
-	sum=0;
-	for(i=mid+1;i<=high;i++)
-	{
-		sum += arry[i];
-		if(sum>right)
-		{
-			right = sum;
-		}else break;
-	}
-
-	return left+right;
-}
-
-int max_sum_subarray(int ar[],int l,int h)
+int max_cross_subarray(int a[],int low,int mid,int high)
 {
 
-	if(l == h)return ar[l];	
-	int m = (l+h)/2;
+    int left_sum = INT_MIN;
+    int max_left;
+    int sum = 0;
+    int i,j;
 
-   /*
-       leftSum  = max_sum_subarray(ar,l,m);
-       rightSum = max_sum_subarray(ar,m+1,h);
-       crossSum = max_cross_sum(ar,l,m,h);
+    for (i=mid; i>=low; i--)
+    {
+        sum = sum+a[i];
+        if (sum>left_sum)
+            left_sum = sum;
+            max_left=i;
+    }
 
-       return max(leftSum,max(rightSum,crossSum));
-    */
-    
-    return max(max_sum_subarray(ar,l,m),max(max_sum_subarray(ar,m+1,h),max_cross_sum(ar,l,m,h)));
-	
+    int right_sum = INT_MIN;
+    int max_right;
+    sum = 0;
+
+     for (j=mid+1; j<=high; j++)
+     {
+           sum=sum+a[j];
+           if (sum>right_sum)
+               right_sum = sum;
+               max_right=j;
+     }
+
+    return (max_left,max_right,left_sum+right_sum);
 }
+int max_subarray(int a[],int low,int high)
+{
+     int mid,left_sum,right_sum,cross_sum;
+
+     if(high==low)
+          return(low,high,a[high]);
+     else
+          mid=(low+high)/2;
+
+     left_sum =max_subarray(a,low,mid);
+     right_sum=max_subarray(a,mid+1,high);
+     cross_sum=max_cross_subarray(a,low,mid,high);
+
+
+     if(left_sum>=right_sum  && left_sum>=cross_sum)
+          return left_sum;
+     else if(right_sum>=left_sum && right_sum>=cross_sum)
+          return right_sum;
+     else
+          return cross_sum;
+
+}
+
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-#endif
 
-	cin>>n;
-	int arry[n+1];
-	for(i=0;i<n;i++)cin>>arry[i];
+   int n,i;
+   cin>>n;
+   int a[n+1];
+   for(i=0;i<n;i++)
+   {
+       cin>>a[i];
+   }
+   int mxsum=max_subarray(a,0,n-1);
+   cout<<"Maximum Contigious Sum : "<<mxsum<<endl;
 
-	cout<<"max sum : "<<max_sum_subarray(arry,0,n-1)<<endl;
 }
 /*
-input : 
+input:
 5
 5 -4 -3 -2 -1
 
-
 output:
-max sum : 5
+Maximum Contigious Sum : 5
 
 */
