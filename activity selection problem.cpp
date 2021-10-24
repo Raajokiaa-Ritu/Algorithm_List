ca0cc96ae1c80b766c8be4f887b02471ab061a41
Raajokiaa-Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2021-10-24 11:35:16 +0600

    Update activity selection problem.cpp

--- activity selection problem.cpp
+++ activity selection problem.cpp
@@ -7,7 +7,6 @@ vector<int>SelectedActivity;
 
 int main()
 {
-
 	cin>>n;
 	int startTime[n+1];
 	int endTime[n+1];
@@ -33,3 +32,11 @@ int main()
 		cout<<"a"<<SelectedActivity[i]<<' ';
 	}
 }
+/*
+9
+1 2 4 1 5 8 9 11 13
+3 5 7 8 9 10 11 14 16
+
+output: 
+Activity: a1 a3 a6 a8 
+*/
